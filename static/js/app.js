ecd7ae87606373c6ca3773c6a617a61bc1291e45
Halfdan Mouritzen <halfdan@robothangarskib.dk>
2013-03-13 14:33:11 +0100

    Quick hack to add context menu

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,15 @@
 $(function() {
     var stage = $('#stage')
 
-    stage.bind('dblclick', function() {
+    stage.bind('dblclick', function(ev) {
         if (stage.hasClass('zoom')) {
             stage.removeClass('zoom')
             return
         }
 
-        stage.append(box())
+        if (ev.target.id == 'stage') {
+            stage.append(box())
+        }
     })
 
     $(window).bind('keydown', function(ev) {
@@ -16,13 +18,19 @@ $(function() {
         }
     })
 
-
     $(window).bind('mousedown', function(ev) {
-        if (ev.which == 2) {
+        if (ev.which == 2) { // Middle click
             stage.toggleClass('zoom')
         }
     })
-});
+
+    $(window).bind('contextmenu', function(ev) {
+        // Create our own :)
+        console.log(ev.target)
+        cmenu(ev)
+        return false
+    })
+})
 
 function box() {
     var box = $('<div class="box"></div>')
@@ -39,4 +47,21 @@ function box() {
     )
 
     return box
+}
+
+function cmenu(ev) {
+    $('#cmenu').remove()
+    var cmenu = $('<div id="cmenu"><ol></ol></div>')
+
+    cmenu.css('top',ev.clientY).css('left', ev.clientX)
+
+    console.log(ev)
+    var menu = cmenu.find('ol')
+    var p1 = $('<li>Create content</li>')
+
+    menu.append(p1)
+
+    $('body').append(cmenu).bind('click', function(ev) {
+        cmenu.remove()
+    })
 }
